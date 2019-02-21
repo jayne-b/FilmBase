60a286225896f291b2b8f28c693541015cf35c54
Jayne Borrer <jayneb@metropolia.fi>
2019-02-21 20:10:24 +0200

    Activities added

---
+++ app/src/main/java/com/example/filmbase/WantActivity.java
@@ -0,0 +1,13 @@
+package com.example.filmbase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_want);
+    }
+}
