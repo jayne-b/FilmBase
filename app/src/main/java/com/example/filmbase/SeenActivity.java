5fc5cff702b0017f2074a71ad30208cb7d65c98f
jayne-b <jayneb@metropolia.fi>
2019-02-21 21:53:33 +0200

    Activities added

---
+++ app/src/main/java/com/example/filmbase/SeenActivity.java
@@ -0,0 +1,13 @@
+package com.example.filmbase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seen);
+    }
+}
